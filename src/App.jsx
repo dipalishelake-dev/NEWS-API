7c6f54c7cdf0856e286b795fa7c4d0c0512b6047
dipalishelake-dev <shelakedipali2002@gmail.com>
2025-07-30 15:38:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from"./components/Navbar"
+import Newsboard from "./components/newsboard"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Newsboard></Newsboard>
+      
+      
+    </div>
+  )
+}
+
+export default App
